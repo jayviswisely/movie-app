36040856fa166e6227f6fcd68ac7f57abbe84795
jayviswisely <104494669+jayviswisely@users.noreply.github.com>
2025-06-20 17:19:03 +0800

    Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search.jsx'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className='pattern'></div>
+
+      <div className='wrapper'>
+        <header>
+          <img src="hero.png" alt="Hero Banner" />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   )
 }
 
