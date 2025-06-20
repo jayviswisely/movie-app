66920b70804ca99f05fd4d2a4fc175f00414e976
jayviswisely <104494669+jayviswisely@users.noreply.github.com>
2025-06-20 20:06:36 +0800

    Add Spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import Search from './components/Search.jsx'
+import Search from './components/Search.jsx';
+import Spinner from './components/Spinner.jsx';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -65,16 +66,16 @@ const App = () => {
         </header>
         
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2 className='mt-[20px]'>All Movies</h2>
 
           {isLoading ? (
-            <p className='text-white'>Loading ...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className='text-red-500'>{errorMessage}</p>
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p className='text-white'>{movie.title}</p>
+                <p key={movie.id} className='text-white'>{movie.title}</p>
               ))}
             </ul>
           )}
